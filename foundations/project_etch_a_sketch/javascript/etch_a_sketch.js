02534a413151da547fe12318bc5857071ba114c3
KaRob121 <karlrobin_espinosa@yahoo.com>
2021-02-20 16:01:39 -0700

    Added color changing feature on mouseover for etch-a-sketch project. Need to work on responsivity.

--- foundations/project_etch_a_sketch/javascript/etch_a_sketch.js
+++ foundations/project_etch_a_sketch/javascript/etch_a_sketch.js
@@ -1,11 +1,26 @@
 function createBoard() {
     // Creates 16 square divs to append to container
-    const container = document.getElementById("container");
+    const container = document.getElementById('container');
     for (let i = 0; i < 16; i++) {
-        div = document.createElement("div");
-        div.setAttribute('class', "square")
-        container.appendChild(div);
+        square = document.createElement('div');
+        square.setAttribute('class','square')
+        squareContent = document.createElement('div');
+        squareContent.setAttribute('class', 'content');
+        square.appendChild(squareContent);
+        container.appendChild(square);
     }
 }
 
-createBoard()
+function changeBackgroundColor(squareContent) {
+    console.log('mouseover')
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    squareContent.style.backgroundColor = "#" + randomColor;
+}
+
+createBoard();
+
+const squareContents = document.querySelectorAll('.content');
+
+squareContents.forEach(squareContent => {
+    squareContent.addEventListener('mouseover', (e) => changeBackgroundColor(squareContent));
+})
