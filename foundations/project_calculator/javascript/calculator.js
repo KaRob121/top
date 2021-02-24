9d8f93974a15fb8f759aceb01d0856850ac2a75c
KaRob121 <karlrobin_espinosa@yahoo.com>
2021-02-24 14:04:11 -0700

    Started TOP, Foundations, Project: Calculator by creating HTML display.

---
+++ foundations/project_calculator/javascript/calculator.js
@@ -0,0 +1,41 @@
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// function addKeyEventListeners(keys) {
+//     keys.forEach((key) => )
+// }
+
+const keys = document.querySelectorAll('.key');
+console.log(keys);
+
+addKeyEventListeners(keys)
