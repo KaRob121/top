d6fbac47d43e07d91d593b8ad699e18aba6ac100
KaRob121 <karlrobin_espinosa@yahoo.com>
2021-02-24 21:23:44 -0700

    Foundations, Project: Calculator - Got calculations to work and have results displayed. Need to fix some bugs with pressing equal button more than once.

--- foundations/project_calculator/javascript/calculator.js
+++ foundations/project_calculator/javascript/calculator.js
@@ -6,10 +6,10 @@ function operate(operator, num1, num2) {
         case '-':
             return subtract(num1, num2);
             break;
-        case '*':
+        case '×':
             return multiply(num1, num2);
             break;
-        case '/':
+        case '÷':
             return divide(num1, num2);
             break;
     }
@@ -31,11 +31,86 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
-// function addKeyEventListeners(keys) {
-//     keys.forEach((key) => )
-// }
+function addKeyEventListeners(keys) {
+    keys.forEach((key) => {
+        if ([...key.classList].includes('num') || document.getElementById('dec') === key) {
+            key.addEventListener('click', (e) => {
+                appendToDisplay(key.textContent);
+            })
+        }
+        else if ([...key.classList].includes('oper')) {
+            // Insert function call to update expr array
+            key.addEventListener('click', (e) => {
+                updateExpression(key.textContent);
+            })
+        }
+        else if (document.getElementById('eq') === key) {
+            // Insert function call to update expr and call operate function
+            key.addEventListener('click', (e) => {
+                evalExpression();
+            })
+        }
+    })
+}
+
+function appendToDisplay(char) {
+    const displayTextNode = document
+        .getElementById('display-text');
+    let displayText = displayTextNode.textContent;
+    let textLength = displayText.length;
+
+    // Clears the display if the expression array is not empty, meaning an operator key has been pressed
+    if (isNewNum) {
+        clearDisplay();
+        isNewNum = false;
+    }
+
+    if (displayText === '0') {
+        // Prevents a number from starting with a decimal
+        if (char !== '.') displayTextNode.textContent = char;
+    }
+    else {
+        // Prevents a decimal from being added more than once
+        if (char === '.') {
+            if (displayText.indexOf('.') === -1) displayTextNode.textContent += char;
+        }
+        else displayTextNode.textContent += char;
+    }
+}
+
+function clearDisplay() {
+    const displayTextNode = document
+        .getElementById('display-text');
+    displayTextNode.textContent = '';
+}
+
+function updateExpression(operator) {
+    const displayText = document
+        .getElementById('display-text')
+        .textContent;
+    
+    expr.push(operator);
+    expr.push(displayText);
+
+    isNewNum = true;
+}
+
+function evalExpression() {
+    const displayTextNode = document.getElementById('display-text');
+    let displayText = document
+        .getElementById('display-text')
+        .textContent;
+
+    console.log(expr)
+    const result = operate(expr[0], +expr[1], +displayText)
+    displayTextNode.textContent = result;
+    expr.length = 0;
+    console.log(expr)
+}
 
 const keys = document.querySelectorAll('.key');
-console.log(keys);
+let expr = [];
+let isNewNum = true;
+
 
 addKeyEventListeners(keys)
