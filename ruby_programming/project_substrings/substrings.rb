9f963b74089be157b22978cc3419b238ed874263
KaRob121 <karlrobin_espinosa@yahoo.com>
2021-03-05 10:59:37 -0700

    Completed TOP, Ruby Programming, Project: Sub Strings.

---
+++ ruby_programming/project_substrings/substrings.rb
@@ -0,0 +1,23 @@
+# Things I learned:
+# I originally used String#include? and Hash#has_key?
+# to implement my solution, but these methods did not account 
+# for duplicates of the substring. I came upon the String#scan method
+# while looking at the solutions of other people (specifically
+# rlmoser and Angel Mitev) for ideas.
+
+# Takes in a string and a dictionary, returning a hash containing 
+# the number of occurences of a substring in the passed string if that 
+# substring is a word in the dictionary.
+def substrings(string, dictionary)
+    substrings = {}
+    string = string.downcase
+    dictionary.each do |word| 
+        num_occurences = string.scan(word).length
+        substrings[word] = num_occurences unless num_occurences == 0 
+    end
+    substrings
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+p substrings("below", dictionary)
+p substrings("Howdy partner, sit down! How's it going?", dictionary)
